818e42f4ae8b5485f24cab046683a2867eb4f773
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-13 23:24:37 +0700

    Начало выполнения дз к теме урока: 'Двумерные массивы и рекурсия' - 5.6

---
+++ Home_work_5.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Home_work_5._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Начало выполнения дз к теме урока: "Двумерные массивы и рекурсия"
+        }
+    }
+}
