b8dcf718b84aba5d89fa7f764ed4da8fe1c579c3
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-14 00:43:29 +0700

    Блок ввода полностью реализован!

--- Home_work_5.6/Program.cs
+++ Home_work_5.6/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Home_work_5._6
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // ДЗ к теме урока: "Двумерные массивы и рекурсия"
             // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
@@ -13,8 +13,21 @@ namespace Home_work_5._6
             Console.Write("Введите количество вводимых чисел: ");
             int M = Convert.ToInt32(Console.ReadLine());
             int[] array = new int[M];
-            
+            int count = 0;
+            FillArray(array);
+            Console.WriteLine($"[{String.Join(";", array)}]");  // Проверка заполненности массива
 
+            void FillArray(int[] arrayInFunction)  // Функция ввода элементов в массив с помощью рекурсивного метода
+            {
+                if (count < arrayInFunction.Length) // условие выхода из рекурсии
+                {
+                    Console.Write($"Введите элемент массива под номером {count} (введите целое число, меньше или больше '0'!): ");
+                    arrayInFunction[count] = Convert.ToInt32(Console.ReadLine());
+                    count++;
+                    FillArray(arrayInFunction);
+                }
+                count = 0;
+            }
         }
     }
 }
