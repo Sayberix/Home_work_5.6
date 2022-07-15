d8b66507c9cc6d0bd7ffe84a91f4fc8b2a672a54
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-15 22:17:41 +0700

    Задача №43 в 5.6.2 - выполнена!

--- Home_work_5.6/Program.cs
+++ Home_work_5.6/Program.cs
@@ -13,7 +13,38 @@ namespace Home_work_5._6
             // b1 = 2, k1 = 5,
             // b2 = 4, k2 = 9   -> (-0,5; -0,5)
 
+            double b1 = 0.0, b2 = 0.0, k1 = 0.0, k2 = 0.0, x = 0.0, y = 0.0;
+            Console.Write("Введите значение b1: ");
+            b1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение b2: ");
+            b2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение k1: ");
+            k1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение k2: ");
+            k2 = Convert.ToInt32(Console.ReadLine());
 
+            if (k1 == k2)        // Проверка, не является ли заданные координаты - одинаковыми или параллельными отрезками
+            {
+                Console.WriteLine("Заданные координаты являются двумя параллельными прямыми или совпадают! Программа завершает работу!");
+                return;
+            }
+
+            x = FindX(b1, b2, k1, k2);
+            y = FindY(b2, k2, x);
+
+            Console.WriteLine($"Точка пересечения заданных двух прямых по заданным координатам - X = {x}; Y = {y}");
+
+            double FindX(double b1, double b2, double k1, double k2)
+            {
+                double x = (-b2 + b1) / (-k1 + k2);
+                return (x);
+            }
+
+            double FindY(double b2, double k2, double x)
+            {
+                double y = k2 * x + b2;
+                return (y);
+            }
         }
     }
 }
